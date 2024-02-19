2cd38845636afe4a3e61245ef7ddf489446173a3
Owen Coughlan <owenc1993@gmail.com>
2024-02-19 22:51:30 +0000

    changes

---
+++ Topic2Statements/saymyname.py
@@ -0,0 +1,13 @@
+# saymyname.py
+# messing with inputs
+# Author Owen Coughlan
+
+'''
+name = input("say your name:")
+print("hello " + name)
+'''
+
+
+number = input("enter a number: ")
+answer = number * 4
+print(answer)
