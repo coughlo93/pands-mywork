2e785e1abc9fda5f0ad68e8d6d9bcdf486086497
Owen Coughlan <owenc1993@gmail.com>
2024-03-08 15:45:34 +0000

    sdgsd

---
+++ Topic6Functions/messing_with_modules.py
@@ -0,0 +1,12 @@
+# messing with modules
+# anaconda has installed a lot of modules already onto the machine
+
+# from constraint import *
+# import nltk
+import math as m
+
+#from math import cos
+
+var = m.cos(3.16)
+
+print (var)
